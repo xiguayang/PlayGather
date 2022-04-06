ea5ea76b402c9c026ec789440cf7c8cfa73d7235
Yang <yangzhangamber1990@gmail.com>
2022-04-06 16:51:42 -0700

    Security issue
    using mongoSanitize to prevent mongo injection; using "sanitize-html"  to prevent xss injections, using helmet  for security

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
+const mongoSanitize = require('express-mongo-sanitize');
 //require our defined error handler class
 const ExpressError = require('./utils_helper/ExpressError');
 const passport = require('passport');
 const localStrategy = require('passport-local');
+const helmet = require('helmet');
+
 const User = require('./models/user');
 
 
@@ -47,8 +50,14 @@ app.set('views',path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
+app.use(mongoSanitize());
+// app.use(
+//     mongoSanitize({
+//         replaceWith: '_',
+//     }),);
 
 const sessionConfig = {
+    name:"session",
     secret:'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
@@ -59,8 +68,56 @@ const sessionConfig = {
     }
 }
 app.use(session(sessionConfig));
-
 app.use(flash());
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+}));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+//This is the array that needs added to
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/cloudyangz/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 //passport middleware
 //passport.session must behind sessionConfig
--- schemas.js
+++ schemas.js
@@ -1,22 +1,45 @@
 //Joi schema api validation
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.playgroundSchema = Joi.object({
     playground:Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
        // zipcode: Joi.number().required().min(5),
         //image: Joi.string().required(),
-        address: Joi.string().required(),
-        description: Joi.string().required(),
-        devices: Joi.string().required(),
-        theme: Joi.string()
+        address: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
+        devices: Joi.string().required().escapeHTML(),
+        theme: Joi.string().escapeHTML()
     }).required(),
     deleteImages:Joi.array()
 })
 
 module.exports.reviewSchema = Joi.object({
     review:Joi.object({
-        rating: Joi.number().required(),
-        body: Joi.string().required()
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required().escapeHTML()
     }).required()
 })
